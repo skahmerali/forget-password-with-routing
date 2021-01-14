275105f019f0c8c57ecdefb12ae7121fc9435c58
SheikhAhmer <sheikhahmerali75@gmail.com>
2021-01-14 11:53:23 +0500

    some code

---
+++ public/app.js
@@ -0,0 +1,119 @@
+
+// const url ="https://signup-database.herokuapp.com/"
+// const url = "http://localhost:3000";
+
+function signup() {
+    axios({
+        method: 'post',
+        url: 'http://localhost:5000/signup',
+        data: {
+            name: document.getElementById("signup-name").value,
+            email: document.getElementById("signup-email").value,
+            password: document.getElementById("signup-password").value,
+            number: document.getElementById("signup-number").value,
+            gender: document.getElementById("signup-gender").value
+        },
+        // alert("tdfthd")
+        // console.log(name)
+        // console.log(email)
+        // console.log(password)
+        // console.log(number)
+        // console.log(gender)
+
+
+        withCredentials: true
+    }).then((response) => {
+        if (response.data.status === 200) {
+            alert(response.data.message)
+            location.href = "./login.html"
+        }
+        else {
+            alert(response.data.message);
+        }
+    }).catch((error) => {
+        console.log(error);
+    });
+    return false
+}
+
+
+
+function userLogin() {
+    axios({
+        method: 'post',
+        url: 'http://localhost:5000/login',
+        data: {
+            email: document.getElementById("login-email").value,
+            passowrd: document.getElementById("login-password").value
+        }, withCredentials: true
+    }).then((response) => {
+        console.log(response);
+        alert(response.data.message)
+        location.href = "./profile.html"
+    }, (error) => {
+        console.log(error);
+    });
+    return false
+}
+// userEmail= document.getElementById("login-email").value;
+// userPassowrd= document.getElementById("login-password").value;
+
+// console.log(obj);
+// const Http = new XMLHttpRequest();
+// Http.open("POST", url + "/login");
+// Http.setRequestHeader("Content-Type", "application/json");
+// Http.send(JSON.stringify(obj))
+// Http.onreadystatechange = (e) => {
+//     if (Http.onreadystate === 4) {
+//         let jsonRes = json.parse(Http.responseText)
+//         console.log(jsonRes);
+//         if (jsonRes === 200) {
+//             alert(jsonRes.message);
+//             alert("login succesfully")
+//         }
+//         else {
+//             alert(jsonRes.message)
+//             alert("sorry! invalid Password or Emmail")
+//         }
+//         window.location.href = "profile.html"
+//     }
+
+// }
+// return false
+
+
+// }
+
+
+
+function getProfile() {
+    axios({
+        method: 'get',
+        url: 'http://localhost:5000/profile',
+        credentials: 'include',
+    }).then((response) => {
+        console.log(response);
+        document.getElementById('print-username').innerHTML = response.data.profile.name
+        document.getElementById('print-email').innerHTML = response.data.profile.email
+        document.getElementById('print-number').innerHTML = response.data.profile.phone
+        document.getElementById('print-gender').innerHTML = response.data.profile.gender
+    }, (error) => {
+        console.log(error.message);
+        location.href = "./login.html"
+    });
+    return false
+    }
+
+    function logout(){
+        axios({
+            method: 'post',
+            url: 'http://localhost:5000/logout',
+        }).then((response) => {
+            console.log(response);
+            location.href = "./login.html"
+        }, (error) => {
+            console.log(error);
+        });
+        return false
+    }
+    
---
+++ route/auth.js
@@ -0,0 +1,140 @@
+var express = require('express')
+var bcrypt = require("bcrypt-inzi")
+var jwt = require('jsonwebtoken');
+var { userModel } = require('../dbconn/modules')
+var router = express.Router();
+var SERVER_SECRET = process.env.SECRET || "1234";
+
+router.post("/signup", (req, res, next) => {
+
+    if (!req.body.name
+        || !req.body.email
+        || !req.body.password
+        || !req.body.phone
+        || !req.body.gender) {
+
+        res.status(403).send(`
+            please send name, email, passwod, phone and gender in json body.
+            e.g:
+            {
+                "name": "Ahmer",
+                "email": "skahmer@gmail.com",
+                "password": "123",
+                "phone": "03462858293",
+                "gender": "Male"
+            }`)
+        return;
+    }
+
+    userModel.findOne({ email: req.body.email },
+        function (err, doc) {
+            if (!err && !doc) {
+
+                bcrypt.stringToHash(req.body.password).then(function (hash) {
+
+                    var newUser = new userModel({
+                        "name": req.body.name,
+                        "email": req.body.email,
+                        "password": hash,
+                        "phone": req.body.phone,
+                        "gender": req.body.gender,
+                    })
+                    newUser.save((err, data) => {
+                        if (!err) {
+                            res.send({
+                                status: 200,
+                                message: "user created"
+                            })
+                        } else {
+                            console.log(err);
+                            res.status(500).send({
+                                message: "user create error, " + err
+                            })
+                        }
+                    });
+                })
+
+            } else if (err) {
+                res.status(500).send({
+                    message: "db error"
+                })
+            } else {
+                res.send({
+                    message: "user already exist"
+                })
+            }
+        })
+
+})
+
+router.post("/login", (req, res, next) => {
+
+    if (!req.body.email || !req.body.password) {
+
+        res.status(403).send(`
+            please send email and passwod in json body.
+            e.g:
+            {
+                "email": "skahmer@gmail.com",
+                "password": "123",
+            }`)
+        return;
+    }
+
+    userModel.findOne({ email: req.body.email },
+        function (err, user) {
+            if (err) {
+                res.status(500).send({
+                    message: "an error occured: " + JSON.stringify(err)
+                });
+            } else if (user) {
+
+                bcrypt.varifyHash(req.body.password, user.password).then(isMatched => {
+                    if (isMatched) {
+                        console.log("matched");
+
+                        var token =
+                            jwt.sign({
+                                id: user._id,
+                                name: user.name,
+                                email: user.email,
+                            }, SERVER_SECRET)
+
+                        res.cookie('jToken', token, {
+                            maxAge: 86_400_000,
+                            httpOnly: true
+                        });
+
+                        res.send({
+                            message: "login success",
+                            user: {
+                                name: user.name,
+                                email: user.email,
+                                phone: user.phone,
+                                gender: user.gender,
+                            }
+                        });
+
+                    } else {
+                        console.log("not matched");
+                        res.status(401).send({
+                            message: "incorrect password"
+                        })
+                    }
+                }).catch(e => {
+                    console.log("error: ", e)
+                })
+
+            } else {
+                res.status(403).send({
+                    message: "user not found"
+                });
+            }
+        });
+})
+router.post("/logout", (req, res, next) => {
+    res.clearCookie('jToken')
+
+    res.send("logout success");
+})
+module.exports = router;
