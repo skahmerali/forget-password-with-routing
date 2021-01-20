be54507c64e5ea5dda771f1cadb3f88b0f457ab2
skahmerali <sheikhahmerali75@gmail.com>
2021-01-20 14:54:49 +0500

    heroku

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 
 // const url = "http://localhost:3000";
-const url='https://login-re-password.herokuapp.com';
+const url='https://login-re-password.herokuapp.com/';
 // const { default: axios } = require("axios");
 var socket = io(url);
 socket.on('connect', function () {
